3af2f0147498894e5d275af7a7aec847a0b7e774
Haruto Fukumoto <runoaima98@gmail.com>
2025-12-29 00:37:03 +0900

    修正

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from "next/app";
 import { AuthProvider } from "@/lib/AuthContext";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <AuthProvider>
             <Component {...pageProps} />
